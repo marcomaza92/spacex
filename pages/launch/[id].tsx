bda453a8c94e408464192bf2dbce674165d12b99
Marco Maza <marcoagustinmaza@gmail.com>
2022-07-07 01:33:47 -0300

    feat: add tablet and desktop UI implementation for launches and launch-detail

--- pages/launch/[id].tsx
+++ pages/launch/[id].tsx
@@ -56,62 +56,68 @@ const Launch = (props) => {
         </div>
       </section>
       <section className={styles.detailSummary}>
-        <div className={styles.detailNumber}>
-          <h4 className={styles.detailNumberTitle}>{rocket.success_rate_pct}%</h4>
-          <p className={styles.detailNumberValue}>Success Rate</p>
-        </div>
-        <div className={styles.detailNumber}>
-          <h4 className={styles.detailNumberTitle}>{rocket.stages}</h4>
-          <p className={styles.detailNumberValue}>Stages</p>
-        </div>
-        <div className={styles.detailNumber}>
-          <h4 className={styles.detailNumberTitle}>{launch_site.site_name}</h4>
-          <p className={styles.detailNumberValue}>Launch Site Name</p>
+        <div className={styles.detailSummaryContainer}>
+          <div className={styles.detailNumber}>
+            <h4 className={styles.detailNumberTitle}>{rocket.success_rate_pct}%</h4>
+            <p className={styles.detailNumberValue}>Success Rate</p>
+          </div>
+          <div className={styles.detailNumber}>
+            <h4 className={styles.detailNumberTitle}>{rocket.stages}</h4>
+            <p className={styles.detailNumberValue}>Stages</p>
+          </div>
+          <div className={styles.detailNumber}>
+            <h4 className={styles.detailNumberTitle}>{launch_site.site_name}</h4>
+            <p className={styles.detailNumberValue}>Launch Site Name</p>
+          </div>
         </div>
       </section>
-      <div>
       <section className={styles.detailContent}>
-        <div className={styles.detailContentHeader}>
-          <h3 className={styles.detailContentHeaderTitle}>About Launched</h3>
-          <p className={styles.detailContentHeaderDescription}>{rocket.description}</p>
-        </div>
-        <div className={styles.detailContentImage}>
-          <img src={rocket.flickr_images[0]} alt="" />
-        </div>
-        <div className={styles.detailContentOverview}>
-          <h3 className={styles.detailContentOverviewTitle}>Overview</h3>
-          <ul className={styles.detailContentOverviewList}>
-            <li className={styles.detailContentOverviewListItem}>
-              <p className={styles.overviewListItemTitle}>Height</p>
-              <p className={styles.overviewListItemValue}>{rocket.height.meters}m <span className={styles.grayText}>/ {rocket.height.feet}ft</span></p>
-            </li>
-            <li className={styles.detailContentOverviewListItem}>
-              <p className={styles.overviewListItemTitle}>Diameter</p>
-              <p className={styles.overviewListItemValue}>{rocket.diameter.meters}m <span className={styles.grayText}>/ {rocket.height.feet}ft</span></p>
-            </li>
-            <li className={styles.detailContentOverviewListItem}>
-              <p className={styles.overviewListItemTitle}>Mass</p>
-              <p className={styles.overviewListItemValue}>{rocket.mass.kg}kg <span className={styles.grayText}>/ {rocket.mass.lb}lb</span></p>
-            </li>
-            {rocket.payload_weights.map((item) => (
-              <li className={styles.detailContentOverviewListItem}>
-                <p className={styles.overviewListItemTitle}>Payload to {item.id}</p>
-                <p className={styles.overviewListItemValue}>{item.kg}kg <span className={styles.grayText}>/ {item.lb}lb</span></p>
-              </li>
-            ))}
-          </ul>
+        <div className={styles.detailContentContainer}>
+          <div className={styles.detailContentHeader}>
+            <h3 className={styles.detailContentHeaderTitle}>About Launched</h3>
+            <p className={styles.detailContentHeaderDescription}>{rocket.description}</p>
+          </div>
+          <div className={styles.flexHelper}>
+            <div className={styles.detailContentImage}>
+              <img src={rocket.flickr_images[0]} alt="" />
+            </div>
+            <div className={styles.detailContentOverview}>
+              <h3 className={styles.detailContentOverviewTitle}>Overview</h3>
+              <ul className={styles.detailContentOverviewList}>
+                <li className={styles.detailContentOverviewListItem}>
+                  <p className={styles.overviewListItemTitle}>Height</p>
+                  <p className={styles.overviewListItemValue}>{rocket.height.meters}m <span className={styles.grayText}>/ {rocket.height.feet}ft</span></p>
+                </li>
+                <li className={styles.detailContentOverviewListItem}>
+                  <p className={styles.overviewListItemTitle}>Diameter</p>
+                  <p className={styles.overviewListItemValue}>{rocket.diameter.meters}m <span className={styles.grayText}>/ {rocket.height.feet}ft</span></p>
+                </li>
+                <li className={styles.detailContentOverviewListItem}>
+                  <p className={styles.overviewListItemTitle}>Mass</p>
+                  <p className={styles.overviewListItemValue}>{rocket.mass.kg}kg <span className={styles.grayText}>/ {rocket.mass.lb}lb</span></p>
+                </li>
+                {rocket.payload_weights.map((item) => (
+                  <li className={styles.detailContentOverviewListItem}>
+                    <p className={styles.overviewListItemTitle}>Payload to {item.id}</p>
+                    <p className={styles.overviewListItemValue}>{item.kg}kg <span className={styles.grayText}>/ {item.lb}lb</span></p>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
         </div>
       </section>
-      </div>
       <section className={styles.detailFooter}>
-        <p className={styles.detailFooterInformation}>For information about our launch services, contact sales@spacex.com</p>
-        <div className={styles.mainButtons}>
-          <p className={styles.mainButtonContainer}>
-            <a href={links.presskit} className={styles.mainButton}>Download User's Guide</a>
-          </p>
-          <p className={styles.mainButtonContainer}>
-            <a href={rocket.wikipedia} className={styles.mainButton}>Capabilities and Services</a>
-          </p>
+        <div className={styles.detailFooterContainer}>
+          <p className={styles.detailFooterInformation}>For information about our launch services, contact sales@spacex.com</p>
+          <div className={styles.mainButtons}>
+            <p className={styles.mainButtonContainer}>
+              <a href={links.presskit} className={styles.mainButton}>Download User's Guide</a>
+            </p>
+            <p className={styles.mainButtonContainer}>
+              <a href={rocket.wikipedia} className={styles.mainButton}>Capabilities and Services</a>
+            </p>
+          </div>
         </div>
       </section>
     </section>
